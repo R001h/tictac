1692280c290cef6195f8673128c7c3e2fce3fc6b
r001h <rortega@fwdcostarica.com>
2024-08-09 10:30:21 -0600

    update commit

--- js/script.js
+++ js/script.js
@@ -17,3 +17,71 @@ function updateScore(){
     localStorage.setItem("marcadorO",marcadorO);
 
 }
+// funcion para limpiar el marcador 
+function clearScore(){
+    marcadorX = 0;
+    marcadorO = 0;
+    updateScore();
+    updateScoreboard();
+}
+
+// funcion para el click en la celda
+function handleClick(event) {
+    if (!gameActivo)
+        return;
+const square = mainbox[index];
+if (square.textContent === ""){
+    square.textContent = playerTurn;
+
+    // cambia color al dar click
+    if (playerTurn === "x"){
+        square.style.backgroundColor ="#f0f0f0";
+    }else {
+        square.style.backgroundColor ="#d0d0d0";
+    }
+    // revisa ganador depues del momviemiento
+    const winner = chechWinner();
+    if (winner) {
+        if (winner === "x"){
+            marcadorX++;
+        }else {
+            marcadorO++;
+        }
+        updateScore();
+        updateScoreboard();
+        alert(winner.toUpperCase()+" ha ganado!" );
+        gameActivo = false;
+        return;
+    }
+    // empate
+     const boardfull = true;
+     for (let i = 0; i < mainbox.length; i++) {
+
+        if (mainbox[i].textContent === ""){
+            boardfull = false;
+            break;
+        }
+     }
+      if (boardfull){
+        alert("Empste !");
+        gameActivo = false;
+        return;
+
+      }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+}
